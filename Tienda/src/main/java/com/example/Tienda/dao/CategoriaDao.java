505d4bed24f5f20c869d2113e73732c744667f27
Chara <164945066+CharaDr39@users.noreply.github.com>
2025-02-23 23:09:52 -0600

    Avance hasta semana 5
    aqui ya logre avanzar hasta lo hecho en la semana 5, lo siguiente que hare son los CRUD de semana 6

---
+++ Tienda/src/main/java/com/example/Tienda/dao/CategoriaDao.java
@@ -0,0 +1,7 @@
+package com.example.Tienda.dao;
+
+import com.example.Tienda.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository<Categoria, Long> {
+}
---
+++ Tienda/src/main/java/com/example/Tienda/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package com.example.Tienda.service;
+
+import com.example.Tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
