73f18eb8089fd9a1e4afa8c1601be873c0f54c4c
Chara <164945066+CharaDr39@users.noreply.github.com>
2025-03-14 20:08:44 -0600

    creacion de carpetas

---
+++ Tienda/src/main/java/com/example/Tienda/domain/Producto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.Tienda.domain;
+
+/**
+ *
+ * @author chara
+ */
+public class Producto {
+
+}
